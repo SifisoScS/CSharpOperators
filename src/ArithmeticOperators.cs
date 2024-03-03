eb4bd95696f5494b622ac54f6ecb98059b58624c
Sifiso Cyprian Shezi <sifiso.cyprianshezi28@gmail.com>
2024-03-03 14:03:48 +0200

    Initial commit

---
+++ src/ArithmeticOperators.cs
@@ -0,0 +1,12 @@
+// Arithmetic Operators
+/*
+    int num1 = 10;
+    int num2 = 5;
+    Console.WriteLine("Arithmetic Operators:");
+    Console.WriteLine($"Addition: {num1 + num2}");
+    Console.WriteLine($"Subtraction: {num1 - num2}");
+    Console.WriteLine($"Multiplication: {num1 * num2}");
+    Console.WriteLine($"Division: {num1 / num2}");
+    Console.WriteLine($"Modulus: {num1 % num2}");
+    Console.WriteLine(); 
+    */
---
+++ src/ComparisonOperators.cs
@@ -0,0 +1,13 @@
+// Comparison Operators
+/*
+    int a = 8;
+    int b = 7;
+    Console.WriteLine("Comparison Operators:");
+    Console.WriteLine($"Equal: {a == b}");
+    Console.WriteLine($"Not Equal: {a != b}");
+    Console.WriteLine($"Greater Than: {a > b}");
+    Console.WriteLine($"Less Than: {a < b}");
+    Console.WriteLine($"Greater Than or Equal To: {a >= b}");
+    Console.WriteLine($"Less Than or Equal To: {a <= b}");
+    Console.WriteLine();
+    */
